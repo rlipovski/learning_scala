958d93dc6ef5ac93badcf63dcdad8d75d55fb607
Roman <roman.lipovski@gmail.com>
2017-06-15 22:11:54 +0200

    refactored the calculateDifference method with parameter groups

--- chapter_4/src/main/scala/PointDifference.scala
+++ chapter_4/src/main/scala/PointDifference.scala
@@ -1,6 +1,6 @@
 
 class PointDifference {
-  def calculateDifference(x1: Int, y1: Int, x2: Int, y2: Int): (Int, Int) = {
+  def calculateDifference(x1: Int, y1: Int)(x2: Int, y2: Int): (Int, Int) = {
     (x1 - x2, y1 - y2)
   }
 }
